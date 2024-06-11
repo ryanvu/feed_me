c137b7db8d51b3ec9dc32dfa10a96e3bf8038f59
Ryan Vu <ryanvu2003@gmail.com>
2024-06-10 22:47:23 -0400

    Font changes - config updates

--- config/prod.exs
+++ config/prod.exs
@@ -7,6 +7,9 @@ import Config
 # before starting your production server.
 config :feed_me, FeedMeWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
+config :feed_me, FeedMe.OpenAi.Openai,
+  api_key: System.get_env("OPENAI_API_KEY")
+
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: FeedMe.Finch
 
